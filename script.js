f4e82cf530e4be9654e06b328e62447210a957cd
EderMoros3 <edermorosrodero@gmail.com>
2025-10-27 13:15:29 +0100

    Creacion pagina contacto + titulo en el index

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function mostrarPantalla() {
       const pantalla = document.getElementById('MenuDesplegable');
       pantalla.classList.toggle('visible');
       const boton = document.getElementById('botonMenuBase');
-      boton.classList.toggle('invisible');
+      boton.classList.toggle('invisible');   
     }
